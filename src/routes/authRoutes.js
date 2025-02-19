9999d16d459bf82a6fb22bef53732709254b69d5
Leandro Sánchez <103838718+leanmsan@users.noreply.github.com>
2025-02-19 15:58:29 -0300

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 router.post('/login', authController.login);
+router.post('/verify-token', authController.verifyToken);
 
 module.exports = router;
